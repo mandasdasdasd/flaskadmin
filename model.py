4dc4a5e6ff830e96a1cf03eece9802ed8ed266f9
Klaus Man <1309584951@qq.com>
2018-07-18 10:10:20 +0800

    forign

--- model.py
+++ model.py
@@ -1,5 +1,14 @@
 # coding=utf-8
+from sqlalchemy_utils.types.choice import ChoiceType
 from . import db
+TYPES = [
+        ('python', 'python'),
+        ('essay', 'eassy'),
+        ('fe', 'fe'),
+        ('linux', 'linux'),
+        ('duanzi', 'duanzi'),
+        ('python', 'python'),
+    ]
 
 
 class Auth(object):
@@ -9,9 +18,31 @@ class Auth(object):
 
 
 class Myblog_list(db.Model):
+
+    __table_name__ = 'myblog_list'
+
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     title = db.Column(db.String(128), nullable=False)
     like = db.Column(db.Integer, nullable=False)
     author = db.Column(db.String(64), nullable=False)
     create_time = db.Column(db.DateTime, nullable=False)
     content = db.Column(db.Text, nullable=False)
+    articleclass = db.relationship('Article_class', backref='articleid')  # backred 允许反向查找
+
+    def __repr__(self):
+        # return '<%r>' % self.category
+        return '%r' % [item for item in self.articleclass["Choice"]]
+
+
+class Article_class(db.Model):
+
+    __table_name__ = 'article_class'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    article_id = db.Column(db.Integer, db.ForeignKey('myblog_list.id'))
+    category = db.Column(ChoiceType(TYPES))
+
+    def __repr__(self):
+        # return '<%r>' % self.category
+        article_obj = self.category
+        return '%s' % article_obj.code
